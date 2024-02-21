36baac6ebf549aab348c344307130c4bc2140b1b
Ilan Pinto <ilpinto@redhat.com>
2024-02-21 12:19:43 +0200

    - supporting multiple doc stores - update gitignore - modified pipeline-v4     - added include evaluation param     - added release flag - replaced default embedding mode to sentence-transformers/all-mpnet-base-v2

--- images/rag/build.sh
+++ images/rag/build.sh
@@ -4,6 +4,6 @@ VERSION=latest # 13
 IMAGE_REPO=quay.io/redhat_emp1
 
 
-docker build --pull -f "images/rag/dockerfile" -t rag-pipeline-tasks:${VERSION} "images/rag"
-docker tag  rag-pipeline-tasks:${VERSION} ${IMAGE_REPO}/rag-pipeline-tasks:${VERSION}
-docker push ${IMAGE_REPO}/rag-pipeline-tasks:${VERSION}
+podman build -f "images/rag/dockerfile" -t rag-pipeline-tasks:${VERSION} "images/rag"
+podman tag  rag-pipeline-tasks:${VERSION} ${IMAGE_REPO}/rag-pipeline-tasks:${VERSION}
+podman push ${IMAGE_REPO}/rag-pipeline-tasks:${VERSION}
